93e3fb366aa2035b94d4bd1c585b773a9171d914
Guilherme Adolpho Josetti Cezar Junior <guilherme.josetti@pucpr.edu.br>
2026-05-18 22:12:49 -0300

    Commit inicial - Guilherme Josetti

---
+++ Player.java
@@ -0,0 +1,18 @@
+class Player
+{
+    private life;
+    private state;
+    private float height, width
+    private float x , y;
+
+    public void Crounch()
+    {
+        //Lógica de agachar
+    }
+
+    public void Jump()
+    {
+        //Lógica de Pulo
+    }
+
+}
